f242cb60e5a579eef8c0dedd9839f11e74fd22ca
LPT-2369 <LPT-2369@LPT2369.newgen.co.in>
2021-06-08 22:54:26 +0530

    abc

--- src/Test/TestCases1.java
+++ src/Test/TestCases1.java
@@ -6,9 +6,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import BrowserLaunch.BrowserFactoryLaunch;
 import Pages.Login;
@@ -16,6 +22,18 @@ import Pages.SignUp;
 
 public class TestCases1 {
 	public WebDriver driver;
+	public ExtentReports report;
+	public ExtentTest test;
+	
+	
+	@BeforeClass
+	public void StartTest()
+	{
+		report = new ExtentReports(System.getProperty("user.dir")+"\\Reports\\ExtentReportResults.html");
+		test = report.startTest("TestExecution");
+		
+		
+	}
 	
 	@BeforeTest
 	public void BrowserLaunch() 
@@ -29,18 +47,37 @@ public class TestCases1 {
   @Test (priority = 0)
   public void loginPage() throws InterruptedException {
 	  Login l = new Login(BrowserFactoryLaunch.driver);
-	  l.login();
-	  
-	
+	 
+	  try {
+		  l.login();
+		  test.log(LogStatus.PASS, "Login Page Executed Successfully");
+		
+	} 
+	  catch (Exception e) {
+		  
+		test.log(LogStatus.FAIL, "Login Failed");
+			
+	}	
 	   
   }
-  
-  
+    
   @Test (priority = 1)
   public void signup() throws InterruptedException {
 	SignUp s=new SignUp(BrowserFactoryLaunch.driver);
-	s.signup();
 		
+	try {
+		
+		s.signup();
+		test.log(LogStatus.PASS, "SignUp Executed successfully");
+		
+	} 
+	   catch (Exception e) {
+		
+		
+		test.log(LogStatus.FAIL, "SignUp FAiled");
+		
+	}
+	
   }
   
   
@@ -50,4 +87,13 @@ public class TestCases1 {
 	  BrowserFactoryLaunch.driver.quit();
 	  
   }
+  
+  @AfterClass
+  public void endtest()
+  
+  {
+	  report.endTest(test);
+	  report.flush();
+  }
+  
 }
